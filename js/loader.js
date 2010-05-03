d3b35d19ee55d564425d61c43a6447864127112c
Tim Heap <heap.tim@gmail.com>
2010-05-03 12:28:26 +1000

    Added String.newlineToBr function.

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,22 @@
+String.implement({
+		newlineToBr: function() {
+			var bits = this.split(/(?:\r|\n|\r\n)/);
+			var html = [];
+
+			bits.each(function(bit) {
+				if (html.length !== 0) {
+					html.push(new Element('br'));
+				}
+				html.push(document.createTextNode(bit));
+			});
+
+			console.log(html);
+			return html;
+		}
+});
+
 window.addEvent('domready', function() {
+	
 	var searchBox, container, feeds;
 	searchBox = new SearchBox($('#searchField'));
 	container = new Container($('#container'));
