8cf21c8ffe15d5883f3f6794fc12797efb114e82
jakekobes <jkobes@utas.edu.au>
2010-05-10 11:37:41 +1000

    updates to the feed classes

--- js/flickr/FlickrFeed.js
+++ js/flickr/FlickrFeed.js
@@ -1,44 +1,97 @@
-var FlickrFeed = new Class({
+/*
+Script: FlickrFeed.js
+	FlickrFeed - MooTools based Flickr feed generator
 
+License:
+	MIT-style license.
+
+Copyright:
+	Copyright (c) 2010 explore.travellr.com
+
+Dependencies:
+	- MooTools-core 1.2.4 or higher
+	- MooTools-more 1.2.4.4 RC1 or higher
+	  - JSONP
+*/
+
+var FlickrFeed = new Class({
     Extends: Feed,
+    Implements: [Options, Events],
+    options: {
+        size: 'm',
+        method: 'flickr.photos.search',
+        amount: 5,
+        apikey: '49dbf1eebc2e9dd4ae02a97d074d83fc'
+    },
+
+    requests: 0,
 
     initialize: function() {},
 
+    // fetch JSON from flickr
     search: function(searchFilter) {
 
-        // TODO: Search for tags individually if nothing is found when searching for them all
+        var tags = [];
+        searchFilter.tags.each(function(tag) {
+            tags.push(tag.tag);
+        });
+        tags = tags.join(',');
 
-                console.log('searchFilter: '+searchFilter.tag);
-        //
-        //        var tags = "";
-        //        searchFilter.tags.each(function(tag) {
-        //            tags = tags + tag + " ";
-        //        });
-
-        new Request.JSONP({
+        this.req = new Request.JSONP({
             url: 'http://api.flickr.com/services/rest/',
             data: {
-                method: 'flickr.photos.search',
-                api_key: '49dbf1eebc2e9dd4ae02a97d074d83fc',
-                format: 'json',
-                tags: '(' + searchFilter.tag + ')',
-                nojsoncallback: 1,
-                per_page: 10
+                api_key: 	this.options.apikey,
+                method: 	this.options.method,
+                per_page: 	this.options.amount,
+                tags:           tags,
+                woe_id:         searchFilter.location.woe_id,
+                format: 	'json'
             },
-            //callbackKey: 'jsoncallback',
-            onComplete: (function(data) {
-                console.log(data);
-                //this.jsonFlickrApi(data);
-                //jsonFlickrApi(data);
-                //jsoncallback=jsonFlickrApi(data);
-                data.photos.photo.each(function(photoData) {
-                    var feedItem = new FlickrFeedItem(photoData);
-                    this.feedItems.push(feedItem);
-                }, this);
-
-                this.feedReady();
-            }).bind(this)
+            callbackKey: 	'jsoncallback',
+            onSuccess: 	this.attachPhoto.bind(this)
         }).send();
+            
+    },
+
+//    // preload all photos
+//    preload: function(photoUrl, details) {
+//        new Asset.image(photoUrl, {
+//            onload: function(photo) {
+//                this.injectPhoto(photo, details);
+//                this.fireEvent('progress', photo);
+//            }.bind(this)
+//        });
+//    },
+//
+//    // inject and display photos
+//    injectPhoto: function(photo, details) {
+//        this.loadCount++;
+//
+//        if(this.options.toImage) this.ref = 'http://farm'+details.farm+'.static.flickr.com/'+details.server+'/'+details.id+'_'+details.secret+'.jpg';
+//        else this.ref = 'http://www.flickr.com/photos/'+details.owner+'/'+details.id;
+//
+//        this.set[this.loadCount] = new Element('a', {
+//            'href': this.ref,
+//            'title': details.title,
+//            'styles': {
+//                'opacity': 0
+//            }
+//        }).grab(photo).inject(this.where).fade('in');
+//
+//        if(this.loadCount == this.response.perpage) this.fireEvent('complete', this.set);
+//    },
+
+    // convert data into URLs
+    attachPhoto: function(response) {
+        
+        this.response = response.photos;
+        response.photos.photo.each(function(photo) {
+            var feedItem = new FlickrFeedItem(photo);
+            this.feedItems.push(feedItem);
+        }, this);
+
+        this.feedReady();
+        
     },
 
     //tesing function for document display
@@ -47,10 +100,4 @@ var FlickrFeed = new Class({
             $(document.body).grab(feedItem.getContent());
         });
     }
-
-//    jsonFlickrApi: function(rsp) {
-//
-//        console.log("ja");
-//    }
-
 });
--- js/flickr/FlickrFeedItem.js
+++ js/flickr/FlickrFeedItem.js
@@ -8,12 +8,12 @@ var FlickrFeedItem = new Class({
     preview: null,
 
     initialize: function(data) {
-       // console.log(data);
-        this.photos = data;
+        //console.log(data);
+        this.photo = data;
 
         //this.question.safe_subject = this.question.subject.toLowerCase().replace(/[^a-z0-9]*/, '-').replace(/^-|-$/, '');
-        this.photos.url = 'http://www.flickr.com/photos/'+this.photos.owner+'/'+this.photos.id;
-        this.photos.picUrl = 'http://farm'+this.photos.farm+'.static.flickr.com/'+this.photos.server+'/'+this.photos.id+'_'+this.photos.secret+'.jpg'
+        this.photo.url = 'http://www.flickr.com/photos/'+this.photo.owner+'/'+this.photo.id;
+        this.photo.picUrl = 'http://farm'+this.photo.farm+'.static.flickr.com/'+this.photo.server+'/'+this.photo.id+'_'+this.photo.secret+'_t.jpg'
         this.size = {
             x: $random(1, 4),
             y: $random(1, 4)
@@ -21,29 +21,48 @@ var FlickrFeedItem = new Class({
     },
 
     makePreview: function() {
-        //console.log("total number is: ");
+       // console.log('makePreview');
         return new Element('div', {
             'class': 'flickr'
         }).adopt([
             new Element('img', {
-                text: this.photos.title,
-                src: this.photos.url
+                text: this.photo.title,
+                src: this.photo.url
                 }),
             ]);
     },
 
+    //TODO fade in the photos
     makeContent: function() {
-        //console.log("dafda");
+       // console.log('makeContent');
         return new Element('div', {
             'class': 'flickr'
         }).adopt([
             new Element('h2').grab(new Element('a', {
-                text: this.photos.title,
-                href: this.photos.url
-                })),
-            new Element('img', {src: this.photos.picUrl, style: 'width:200px; height: 200px; border:3px solid red'}
-    )]);
+                text: this.photo.title,
+                href: this.photo.url
+            })),
+            new Element('a', {href: this.photo.url}).grab(new Element('img', {
+                src: this.photo.picUrl,
+                style: 'width:100px; height: 100px; border:3px solid red'
+            }))
+        ]);
 
+    },
+
+    // inject and display photos
+    injectPhoto: function() {
+        this.loadCount++;
+
+        this.set[this.loadCount] = new Element('a', {
+            'href': this.photo.picUrl,
+            'title': this.photo.title,
+            'styles': {
+                'opacity': 0
+            }
+        }).grab(this.photo).inject('flickr').fade('in');
+
+        if(this.loadCount == 1) this.fireEvent('complete', this.set);
     }
 
 });
