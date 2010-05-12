1ed1ed09f5db457f1fb1c5432a7968de0af09ffc
Tim Heap <heap.tim@gmail.com>
2010-05-12 22:35:18 +1000

    Cleaned up the Twitter feed and feed item
    Superfluous code was removed. Link fixed. Updated to use Travellrs
    actual data instead of testing data.

--- js/twitter/TwitterFeed.js
+++ js/twitter/TwitterFeed.js
@@ -3,39 +3,38 @@ var TwitterFeed = new Class({
 
     PER_PAGE: 3,
 
-initialize: function() {},
-    search: function(searchFilter) {
+    name: 'Twitter',
 
+    search: function(searchFilter) {
+        this.parent();
         // TODO: Search for tags individually if nothing is found when searching for them all
 
         var tags = "";
         searchFilter.tags.each(function(tag) {
-            tags = tags + tag.stub + " ";
+            tags = tags + tag.name + " ";
         });
 
-       // TODO: Check whether the search query contains an activity or location and search accordingly
+        // TODO: Check whether the search query contains an activity or location and search accordingly
         new Request.JSONP({
             url: 'http://search.twitter.com/search.json',
             data: {
-                geo_code: searchFilter.location.lat + "," + searchFilter.location.lng +",1mi",
-                q: tags,
+                geo_code: (searchFilter.location ? searchFilter.location.lat + "," + searchFilter.location.lng +",1mi" : null),
+                q: searchFilter.searchString,
+                lang: 'en',
                 page: 1,
-                rpp: TwitterFeed.PER_PAGE
+                rpp: this.PER_PAGE,
+                result_type: 'recent' //results can also be popular or mixed
             },
             onComplete: (function(data) {
-                console.log(data);
-                /*
-                data.each(function(questionData) {
-                    var feedItem = new TravellrFeedItem(questionData);
-                    this.feedItems.push(feedItem);
-                }, this);
+                if (data.results && $chk(data.results.length)) {
+                    data.results.each(function(data) {
+                        var feedItem = new TwitterFeedItem(data);
+                        this.feedItems.push(feedItem);
+                    }, this);
+                }
 
                 this.feedReady();
-                */
-            }).bind(this),
-            onFailure: function() {
-                console.log(arguments);
-            }
+            }).bind(this)
 
         }).send();
     }
--- js/twitter/TwitterFeedItem.js
+++ js/twitter/TwitterFeedItem.js
@@ -7,11 +7,12 @@ var TwitterFeedItem = new Class({
     content: null,
     preview: null,
 
+    name: 'Twitter',
+
     initialize: function(data) {
         this.text = data;
 
-        this.text = this.text.toLowerCase().replace(/[^a-z0-9]*/, '-').replace(/^-|-$/, '');
-        this.text.url = 'http://search.twitter.com/.json?q=' + this.text.q;
+        this.text.url = 'http://twitter.com/' + this.text.from_user + '/status/' + this.text.id;
 
         this.size = {
             x: $random(1, 4),
@@ -22,41 +23,26 @@ var TwitterFeedItem = new Class({
     makePreview: function() {
         return new Element('div', {
             'class': 'twitter'
-        }).adopt([
+        }).grab(new Element('div', {'class': 'inner'}).adopt([
             new Element('p', {
-                text: this.text.subject
+                text: this.text.text
             }),
-            ]);
-            
+        ]));
     },
-    onComplete: (function(data) {
-        console.log(data);
-    /*
-                data.each(function(questionData) {
-                    var feedItem = new TravellrFeedItem(questionData);
-                    this.feedItems.push(feedItem);
-                }, this);
 
-                this.feedReady();
-                */
-    }).bind(this),
-    onFailure: function() {
-        console.log(arguments);
-
-            makeContent: function() {
-                return new Element('div', {
-                    'class': 'twitter'
-                }).adopt([
-                    new Element('h2').grab(new Element('a', {
-                        text: this.text.from_user,
-                        href: this.text.url
-                    })),
-                    new Element('p').adopt(this.text.content.newlineToBr()),
-                    new Element('p', {
-                        'class': 'date',
-                        text: Date.parse(this.text.created_at).toString()
-                    })
-                    ]);
-            }
+    makeContent: function() {
+        return new Element('div', {
+            'class': 'twitter'
+        }).adopt([
+            new Element('h2').grab(new Element('a', {
+                text: this.text.from_user,
+                href: this.text.url
+            })),
+            new Element('p', {text: this.text.text}),
+            new Element('p', {
+                'class': 'date',
+                text: Date.parse(this.text.created_at).toString()
+            })
+        ]);
     }
 });
