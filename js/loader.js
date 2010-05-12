12e398be23b951b5b4d8293f53fb87ee8465961e
Tim Heap <heap.tim@gmail.com>
2010-05-12 22:58:08 +1000

    Added flickr feed to loader script

--- js/loader.js
+++ js/loader.js
@@ -14,15 +14,32 @@ String.implement({
     }
 });
 
+Array.implement({
+	getRandomKey: function() {
+		return $random(0, this.length - 1);
+	},
+	removeRandom: function() {
+		var index = this.getRandomKey();
+		    splice = this.splice(index, 1);
+		return splice[0];
+	}
+});
+
 window.addEvent('domready', function() {
 
-    var searchBox = new SearchBox('searchBox');
+    var searchBox = new SearchBox('searchField');
     var container = new Container('container', searchBox);
 
     //feeds = [TravellrFeed, WorldNomadsFeed, TwitterFeed, FlickrFeed, WikiTravelFeed];
-    var feeds = [TravellrFeed, TwitterFeed];
+    var feeds = [TravellrFeed, TwitterFeed, FlickrFeed];
     feeds.each(function(AFeedClass) {
         new AFeedClass(searchBox, container);
     });
     
+    var searchString = location.href.toURI().getData('search').replace(/\+/g, ' ');
+    if (searchString) {
+        $('searchField').set('value', searchString);
+        searchBox.search(searchString);
+    }
+        
 });
