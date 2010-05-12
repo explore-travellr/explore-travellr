7e914fd6e98748be406916d0a509b258bfd0e79d
Tim Heap <heap.tim@gmail.com>
2010-05-12 22:27:51 +1000

    Cleaned up the Flickr feed and feed item

--- js/flickr/FlickrFeed.js
+++ js/flickr/FlickrFeed.js
@@ -1,17 +1,17 @@
 /*
 Script: FlickrFeed.js
-	FlickrFeed - MooTools based Flickr feed generator
+   FlickrFeed - MooTools based Flickr feed generator
 
 License:
-	MIT-style license.
+   MIT-style license.
 
 Copyright:
-	Copyright (c) 2010 explore.travellr.com
+   Copyright (c) 2010 explore.travellr.com
 
 Dependencies:
-	- MooTools-core 1.2.4 or higher
-	- MooTools-more 1.2.4.4 RC1 or higher
-	  - JSONP
+   - MooTools-core 1.2.4 or higher
+   - MooTools-more 1.2.4.4 RC1 or higher
+   - JSONP
 */
 
 var FlickrFeed = new Class({
@@ -26,78 +26,46 @@ var FlickrFeed = new Class({
 
     requests: 0,
 
-    initialize: function() {},
+    name: 'Flickr',
 
     // fetch JSON from flickr
     search: function(searchFilter) {
-
+        this.parent();
         var tags = [];
+
         searchFilter.tags.each(function(tag) {
-            tags.push(tag.tag);
+            tags.push(tag.name);
         });
         tags = tags.join(',');
 
         this.req = new Request.JSONP({
             url: 'http://api.flickr.com/services/rest/',
-            data: {
+                data: {
                 api_key: 	this.options.apikey,
                 method: 	this.options.method,
                 per_page: 	this.options.amount,
-                tags:           tags,
-                woe_id:         searchFilter.location.woe_id,
+                tags:           tags+', travel',
+                woe_id:         (searchFilter.location ? searchFilter.location.woe_id : null),
                 format: 	'json'
             },
             callbackKey: 	'jsoncallback',
-            onSuccess: 	this.attachPhoto.bind(this)
+            onSuccess: 	this.makeFeedItems.bind(this)
         }).send();
-            
+
     },
 
-//    // preload all photos
-//    preload: function(photoUrl, details) {
-//        new Asset.image(photoUrl, {
-//            onload: function(photo) {
-//                this.injectPhoto(photo, details);
-//                this.fireEvent('progress', photo);
-//            }.bind(this)
-//        });
-//    },
-//
-//    // inject and display photos
-//    injectPhoto: function(photo, details) {
-//        this.loadCount++;
-//
-//        if(this.options.toImage) this.ref = 'http://farm'+details.farm+'.static.flickr.com/'+details.server+'/'+details.id+'_'+details.secret+'.jpg';
-//        else this.ref = 'http://www.flickr.com/photos/'+details.owner+'/'+details.id;
-//
-//        this.set[this.loadCount] = new Element('a', {
-//            'href': this.ref,
-//            'title': details.title,
-//            'styles': {
-//                'opacity': 0
-//            }
-//        }).grab(photo).inject(this.where).fade('in');
-//
-//        if(this.loadCount == this.response.perpage) this.fireEvent('complete', this.set);
-//    },
+    makeFeedItems: function(response) {
 
-    // convert data into URLs
-    attachPhoto: function(response) {
-        
         this.response = response.photos;
-        response.photos.photo.each(function(photo) {
-            var feedItem = new FlickrFeedItem(photo);
-            this.feedItems.push(feedItem);
-        }, this);
+        //console.log(response);
+        if($chk(this.response)) {
+            response.photos.photo.each(function(photo) {
+                var feedItem = new FlickrFeedItem(photo);
+                this.feedItems.push(feedItem);
+            }, this);
+        }
 
         this.feedReady();
-        
-    },
 
-    //tesing function for document display
-    feedReady: function() {
-        this.feedItems.each(function(feedItem) {
-            $(document.body).grab(feedItem.getContent());
-        });
     }
 });
--- js/flickr/FlickrFeedItem.js
+++ js/flickr/FlickrFeedItem.js
@@ -8,12 +8,14 @@ var FlickrFeedItem = new Class({
     preview: null,
 
     initialize: function(data) {
-        //console.log(data);
         this.photo = data;
 
-        //this.question.safe_subject = this.question.subject.toLowerCase().replace(/[^a-z0-9]*/, '-').replace(/^-|-$/, '');
         this.photo.url = 'http://www.flickr.com/photos/'+this.photo.owner+'/'+this.photo.id;
-        this.photo.picUrl = 'http://farm'+this.photo.farm+'.static.flickr.com/'+this.photo.server+'/'+this.photo.id+'_'+this.photo.secret+'_t.jpg'
+        this.photo.picUrlThumbnail = 'http://farm'+this.photo.farm+'.static.flickr.com/'+this.photo.server+'/'+this.photo.id+'_'+this.photo.secret+'_m.jpg';
+        this.photo.picUrlContent = 'http://farm'+this.photo.farm+'.static.flickr.com/'+this.photo.server+'/'+this.photo.id+'_'+this.photo.secret+'.jpg';
+
+        new Asset.images([this.photo.picUrlThumbnail, this.photo.picUrlContent]);
+
         this.size = {
             x: $random(1, 4),
             y: $random(1, 4)
@@ -21,48 +23,25 @@ var FlickrFeedItem = new Class({
     },
 
     makePreview: function() {
-       // console.log('makePreview');
         return new Element('div', {
             'class': 'flickr'
-        }).adopt([
+        }).grab(new Element('div', {'class': 'inner'}).adopt([
             new Element('img', {
                 text: this.photo.title,
-                src: this.photo.url
-                }),
-            ]);
+                src: this.photo.picUrlThumbnail,
+            }),
+        ]));
     },
 
-    //TODO fade in the photos
     makeContent: function() {
-       // console.log('makeContent');
-        return new Element('div', {
-            'class': 'flickr'
-        }).adopt([
+        return new Element('div', {'class': 'flickr'}).adopt([
             new Element('h2').grab(new Element('a', {
                 text: this.photo.title,
                 href: this.photo.url
             })),
             new Element('a', {href: this.photo.url}).grab(new Element('img', {
-                src: this.photo.picUrl,
-                style: 'width:100px; height: 100px; border:3px solid red'
+                src: this.photo.picUrlContent
             }))
         ]);
-
-    },
-
-    // inject and display photos
-    injectPhoto: function() {
-        this.loadCount++;
-
-        this.set[this.loadCount] = new Element('a', {
-            'href': this.photo.picUrl,
-            'title': this.photo.title,
-            'styles': {
-                'opacity': 0
-            }
-        }).grab(this.photo).inject('flickr').fade('in');
-
-        if(this.loadCount == 1) this.fireEvent('complete', this.set);
     }
-
 });
