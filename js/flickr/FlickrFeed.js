629c54c202ae78c82af87d74ab3bc2d8ab8374d8
maelstrom <heap.tim@gmail.com>
2010-07-19 11:25:57 +1000

    NaturalDocs comments for flickr

--- js/flickr/FlickrFeed.js
+++ js/flickr/FlickrFeed.js
@@ -1,6 +1,9 @@
 /*
-Script: FlickrFeed.js
-   FlickrFeed - MooTools based Flickr feed generator
+Class: flickr.FlickrFeed
+Fetches feed data from Flickr photo streams
+
+Extends:
+    <Feed>
 
 License:
    MIT-style license.
@@ -9,35 +12,55 @@ Copyright:
    Copyright (c) 2010 explore.travellr.com
 
 Dependencies:
-   - MooTools-core 1.2.4 or higher
-   - MooTools-more 1.2.4.4 RC1 or higher
-   - Request/Request.JSONP
-   - Feed Class
-   - FlickrFeedItem Class
+   - <MooTools::core> 1.2.4 or higher
+   - <MooTools::more> 1.2.4.4 RC1 or higher
+   - <MooTools::more> Request.JSONP
+   - <Feed>
+   - <FlickrFeedItem>
 */
 
 var FlickrFeed = new Class({
     
+    Implements: [Options, Events],
     Extends: Feed,
 
+    /**
+     * Variable: itemsCalled
+     * The number of results displayed
+     */
     itemsCalled: null,
 
-    Implements: [Options, Events],
+    /**
+     * Variable: options
+     * A <JS::Object> containing options for <FlickrFeeds>
+     *
+     *   size - The size of the content image
+     *   method - The API method to call to search images
+     *   apikey - The Flickr API key to use
+     */
     options: {
         size: 'm',
         method: 'flickr.photos.search',
         apikey: '49dbf1eebc2e9dd4ae02a97d074d83fc'
     },
 
+    // TODO Investigate if this is used
     requests: 0,
 
+    /**
+     * Variable: name
+     * The name of this <Feed>, for use in the GUI
+     */
     name: 'Flickr',
 
     /**
+     * Function: search
      * Search the feed for items relating to the search terms. Calls
      * makeFeedItems on success.
      *
-     * @param searchFilter The search filter to filter results with
+     * Paramaters:
+     *
+     *     searchFilter - The search filter to filter results with
      */
     search: function(searchFilter) {
         this.parent();
@@ -54,25 +77,27 @@ var FlickrFeed = new Class({
         new Request.JSONP({
             url: 'http://api.flickr.com/services/rest/',
                 data: {
-                api_key: 	this.options.apikey,
-                method: 	this.options.method,
-                per_page: 	this.itemsCalled,
-                tags:           tags,
-                woe_id:         (searchFilter.location ? searchFilter.location.woe_id : null),
-                format: 	'json',
-                sort:           'relevance'
+                api_key: this.options.apikey,
+                method: this.options.method,
+                per_page: this.itemsCalled,
+                tags: tags,
+                woe_id: (searchFilter.location ? searchFilter.location.woe_id : null),
+                format: 'json',
+                sort: 'relevance'
             },
-            callbackKey: 	'jsoncallback',
-            onSuccess: 	this.makeFeedItems.bind(this)
+            callbackKey: 'jsoncallback',
+            onSuccess: this.makeFeedItems.bind(this)
         }).send();
     },
 
     /**
-     * Makes the individual flickr feed items by sending the each photo
-     * object of the response object to the FlickrFeedItem class and then
-     * pushing each of them onto a feedItems array
+     * Function: makeFeedItems
+     * Makes the individual <FlickrFeedItems> by sending the each photo
+     * object of the response object to the <FlickrFeedItem> class and then
+     * pushing each of them onto the <Feed::feedItems> array
      *
-     * @param response object returned by the flickr call
+     * Paramaters:
+     *     response - object returned by the flickr call
      */
     makeFeedItems: function(response) {
 
